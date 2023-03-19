d8f3f323f61b58ae818ace1b1c3f116a6ba77729
Gaus Al Munir Tushar <tushar01910533370@gmail.com>
2023-03-19 14:29:44 +0600

    login controller completed , bcrypt and jwt added

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/userModel')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     const {email} = req.body
@@ -30,8 +30,29 @@ const register = async (req, res) => {
     }
 }
 
-const login = () => {
-
+const login = async (req, res) => {
+    try {
+        const user = await userModel.findOne({email: req.body.email})
+        if(!user){
+            return res.status(200).send({
+                message: 'User not Found'
+            })
+        }
+        const isMatch = await bcrypt.compare(req.body.password, user.password)
+        if(!isMatch){
+            return res.status(200).send({
+                message: 'Invalid Email or Password'
+            })
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'})
+        res.status(200).send({message: 'Login Success', success: true, token})
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            message: `${error.message}`
+        })
+    }
 }
 
 module.exports = {
